88d54939ed133a77a011f5d0978ce9d4c4067b53
Vasily Parshentsev <vparshentsev@gmail.com>
2022-10-15 19:28:08 +0300

    Initial commit

---
+++ src/Worker.java
@@ -0,0 +1,29 @@
+public class Worker {
+    @FunctionalInterface
+    public interface OnTaskDoneListener {
+        void onDone(String result);
+    }
+
+    @FunctionalInterface
+    public interface OnTaskErrorListener {
+        void onError(String result);
+    }
+
+    private OnTaskDoneListener callback;
+    private OnTaskErrorListener errorCallback;
+
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+        this.callback = callback;
+        this.errorCallback = errorCallback;
+    }
+
+    public void start() {
+        for (int i = 0; i < 100; i++) {
+            if (i != 33) {
+                callback.onDone("Task " + i + " is done");
+            } else {
+                errorCallback.onError("Error! Task " + i + " is undone");
+            }
+        }
+    }
+}
